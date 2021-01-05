11f72cf91c34f35cc27408d54c6e7b69d3c8665d
Jeremy Oden <jerem.oden@gmail.com>
2021-01-05 22:43:27 +0100

    Add Triggers section.

--- src/Main.vue
+++ src/Main.vue
@@ -21,48 +21,98 @@
           </div>
         </div>
       </section>
+  
       <div class="container has-text-centered">
         <h1 class="title">Software</h1>
       </div>
-      <div class="columns has-text-justified">
-        <div class="column">
-          <section class="section">
-            eXaDrums software is available from some Debian-based Linux distributions, including: Debian, Ubuntu, and Raspberry Pi OS.
-            Including software installation guidelines to your custom user guide gives you the choice of installing the official version of eXaDrums, 
-            using your OS package manager, or build the latest version yourself.
-          </section>
+      <div class="container">
+        <div class="columns has-text-justified">
+          <div class="column">
+            <section class="section">
+              eXaDrums software is available from some Debian-based Linux distributions, including: Debian, Ubuntu, and Raspberry Pi OS.
+              Including software installation guidelines to your custom user guide gives you the choice of installing the official version of eXaDrums, 
+              using your OS package manager, or build the latest version yourself.
+            </section>
+          </div>
+          <div class="column">
+            <section class="section">
+              <div class="field">
+                  <b-switch v-model="software_guidelines">Include software installation guidelines.</b-switch><br><br>
+                  <span v-if="software_guidelines">Software installation method: </span>
+                  <b-field v-if="software_guidelines">
+                    <b-radio-button v-model="installation_method" native-value="package_manager">
+                        <b-icon icon="box-open"></b-icon>
+                        <span>Package manager</span>
+                    </b-radio-button>
+                    <b-radio-button v-model="installation_method" native-value="from_source">
+                        <b-icon icon="cogs"></b-icon>
+                        <span>Build from source</span>
+                    </b-radio-button>
+                  </b-field>
+              </div>
+            </section>
+          </div>
         </div>
-        <div class="column">
-          <section class="section">
-            <div class="field">
-                <b-switch v-model="software_guidelines">Include software installation guidelines.</b-switch><br><br>
-                <span v-if="software_guidelines">Software installation method: </span>
-                <b-field v-if="software_guidelines">
-                  <b-radio-button v-model="installation_method" native-value="package_manager">
-                      <b-icon icon="box-open"></b-icon>
-                      <span>Package manager</span>
+      </div>
+
+      <div class="container has-text-centered">
+        <h1 class="title">Triggers</h1>
+      </div>
+      <div class="container">
+        <div class="columns has-text-justified">
+          <div class="column">
+            <section class="section">
+              At the moment, you can only get 8 triggers, but in a near future there will be another option to get 16 triggers.
+            </section>
+          </div>
+          <div class="column">
+            <section class="section">
+              <div class="field">
+                Number of triggers: 
+                <b-field>
+                  <b-radio-button v-model="triggers_number" native-value="8">
+                      <b-icon icon="drum"></b-icon>
+                      <span>8 Triggers</span>
                   </b-radio-button>
-                  <b-radio-button v-model="installation_method" native-value="from_source">
-                      <b-icon icon="cogs"></b-icon>
-                      <span>Build from source</span>
+                  <b-radio-button v-model="triggers_number" native-value="16" disabled>
+                      <b-icon icon="drum"></b-icon>
+                      <span>16 Triggers</span>
                   </b-radio-button>
                 </b-field>
-            </div>
-          </section>
+              </div>
+            </section>
+          </div>
         </div>
       </div>
-      <div class="columns">
-        <div class="column"> 
-          <section class="section">
-            <!-- <span @click="clickMe">
-              <qrcode-vue :value="value" :size="size" level="H" renderAs="canvas"></qrcode-vue>
-            </span>
-            <input type="text" name="" id="" v-model="value" size="40"> -->
-          </section>
+
+      <section class="section">
+        <div class="container has-text-justified">
+          <h1 class="title">Get Ready!</h1>
+          <div class="block">
+            Click the "Generate" button to generate your personal user guide.
+            It will be available from the generated link and QR code.
+            If you wish to make modifications, you can update the QR code and link by clicking the "Update" button.
+          </div>
         </div>
-        <div class="column">
+      </section>
+      <div class="container">
+        <div class="columns has-text-justified">
+          <div class="column">
+            <section class="section">
+              <b-button :label="generate_label"></b-button>
+            </section>
+          </div>
+          <div class="column">
+            <section class="section">
+              <span @click="clickMe">
+                <qrcode-vue :value="value" :size="size" level="H" renderAs="canvas"></qrcode-vue>
+              </span>
+              {{value}}
+            </section>
+          </div>
         </div>
       </div>
+
       <footer class="footer">
         <div class="content has-text-centered">
           <p>
@@ -82,8 +132,10 @@
             return {
                 installation_method: 'package_manager',
                 value: 'https://make.exadrums.com/?id=123',
-                size: 100,
-                software_guidelines: false
+                size: 120,
+                software_guidelines: true,
+                triggers_number: '8',
+                generate_label: 'Generate Custom User Guide'
             }
         },
         methods:
